34a2462c84f57d0bb76876f62c298da420718ff1
Nanda Kumar - home <nanda@kiluvai.com>
2023-11-17 01:16:05 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react' 
+import './App.css'
+import {heading} from '../src/components/heading/heading';
+
+function App() { 
+
+  return (
+    <>
+      <heading />
+    </>
+  )
+}
+
+export default App
